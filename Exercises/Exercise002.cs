47302e7eeba53e41771619ac3fd2b1c61e1c1bce
anaghakarurkar <anaghakarurkar@gmail.com>
2022-08-24 15:03:23 +0100

    Implemented two methods to check if person is from manchester and if person can watch a film

--- Exercises/Exercise002.cs
+++ Exercises/Exercise002.cs
@@ -1,26 +1,18 @@
 ﻿using System;
+using Exercises.Models;
+
 namespace Exercises
 {
     public class Exercise002
     {
-        // 👉 Remember to delete all unnecessary comments (like this one) when you're done! 
-
-        public bool IsFromManchester(/* Person person */)
+        public bool IsFromManchester( Person person)
         {
-            // 👉 DON'T FORGET to uncomment the tests in Exercise002Tests.cs 
-            // Uncomment the parameter above and delete this comment
-
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return (person != null && person.City.Equals("Manchester"));
         }
 
-        public bool CanWatchFilm(/*Person person, int ageLimit*/)
+        public bool CanWatchFilm(Person person, int ageLimit)
         {
-            // 👉 DON'T FORGET to uncomment the tests in Exercise002Tests.cs 
-            // Uncomment the parameters above and delete this comment
-
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return person.Age >= 18;
         }
     }
 }
